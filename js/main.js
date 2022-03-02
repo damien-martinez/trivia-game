104b70c8b6635cb9bafcd3138f062d98361c6ab1
damien-martinez <mart.damien1@gmail.com>
2022-03-02 19:24:09 +0000

    Completed feature 1 and made changes based on feedback.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var categoriesRequest = new XMLHttpRequest();
 var questionsRequest = new XMLHttpRequest();
 var $categorySelect = document.querySelector('#category-select');
-var category = '';
-// var $triviaButton = document.querySelector('.play-trivia');
-
 var $triviaForm = document.querySelector('#trivia-form');
+var $questionContainer = document.querySelector('.question-container');
+var $setUpContainer = document.querySelector('.container');
+
+var category = '';
 
 categoriesRequest.open('GET', 'https://opentdb.com/api_category.php');
 
@@ -26,31 +27,67 @@ categoriesRequest.send();
 
 function submitForm(event) {
   event.preventDefault();
-  // var playerName = $triviaForm.elements.name.value;
   var difficulty = $triviaForm.elements.difficulty_select.value;
   var numberOfQuestions = $triviaForm.elements.number_of_questions.value;
   var pulledCategory = $triviaForm.elements.category_select.value;
 
   if (difficulty === '') {
-    var apiUrl = 'https://opentdb.com/api.php?' + 'amount=' + numberOfQuestions + '&category=' + pulledCategory;
+    var apiUrl = 'https://opentdb.com/api.php?' + 'amount=' + numberOfQuestions + '&category=' + pulledCategory + '&type=multiple';
   } else {
-    apiUrl = 'https://opentdb.com/api.php?' + 'amount=' + numberOfQuestions + '&category=' + pulledCategory + '&difficulty=' + difficulty;
+    apiUrl = 'https://opentdb.com/api.php?' + 'amount=' + numberOfQuestions + '&category=' + pulledCategory + '&difficulty=' + difficulty + '&type=multiple';
   }
-
-  // console.log(apiUrl);
 
   questionsRequest.open('GET', apiUrl);
   questionsRequest.responseType = 'json';
 
-  // function getTrivia() {
-  //   var triviaObj = questionsRequest.response;
+  function getTrivia() {
+    var triviaObj = questionsRequest.response;
 
-  // }
+    if (questionsRequest.status === 200) {
+      seeQuestions(triviaObj);
+    }
 
-  // questionsRequest.addEventListener('load', getTrivia);
+  }
 
-  // questionsRequest.send();
+  questionsRequest.addEventListener('load', getTrivia);
+
+  questionsRequest.send();
 
 }
 
 $triviaForm.addEventListener('submit', submitForm);
+
+function seeQuestions(triviaObj) {
+  $setUpContainer.setAttribute('class', 'hidden');
+  $questionContainer.setAttribute('class', 'question-container');
+  var results = triviaObj.results;
+
+  for (var i = 0; i < results.length; i++) {
+
+    var $paragraph = document.createElement('p');
+    $paragraph.textContent = 'Question: ' + results[i].question;
+
+    var $questionAnswerDiv = document.createElement('div');
+    $questionAnswerDiv.appendChild($paragraph);
+    $questionContainer.appendChild($questionAnswerDiv);
+
+    var $choice1 = document.createElement('p');
+    var $choice2 = document.createElement('p');
+    var $choice3 = document.createElement('p');
+    var $choice4 = document.createElement('p');
+
+    $choice1.textContent = 'A. ' + results[i].correct_answer;
+    $choice2.textContent = 'B. ' + results[i].incorrect_answers[0];
+    $choice3.textContent = 'C. ' + results[i].incorrect_answers[1];
+    $choice4.textContent = 'D. ' + results[i].incorrect_answers[2];
+
+    $questionAnswerDiv.appendChild($choice1);
+    $questionAnswerDiv.appendChild($choice2);
+    $questionAnswerDiv.appendChild($choice3);
+    $questionAnswerDiv.appendChild($choice4);
+
+    $questionContainer.appendChild($questionAnswerDiv);
+
+  }
+
+}
